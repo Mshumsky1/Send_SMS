38a8b651a43a303a6cae241822b03fb6e1dad2df
Mshumsky1 <112615574+Mshumsky1@users.noreply.github.com>
2023-06-25 15:19:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -19,11 +19,20 @@ async function sendSMS(to, name) {
       const responseData = await response.json();
       console.log('Message sent successfully');
       console.log(responseData);
+      return true;
     } catch (error) {
       console.log('There was an error sending the message.');
       console.error(error);
+      return false;
     }
   }
+  
+  function isValidPhoneNumber(phone) {
+    // Updated regular expression to match the format 81-70-8959-5295
+    const phoneNumberRegex = /^\d{2}\d{2}\d{4}\d{4}$/;
+    return phoneNumberRegex.test(phone);
+  }
+  
   
   // Add an event listener to the form submission
   const form = document.getElementById('contact-form');
@@ -33,5 +42,17 @@ async function sendSMS(to, name) {
     const name = form.name.value;
     const phone = form.phone.value;
   
-    await sendSMS(phone, name);
+    if (!isValidPhoneNumber(phone)) {
+      alert('Please enter a valid phone number.');
+      return;
+    }
+  
+    const success = await sendSMS(phone, name);
+  
+    const messageElement = document.getElementById('message');
+    if (success) {
+      messageElement.textContent = 'Thanks, check your phone for a code';
+    } else {
+      messageElement.textContent = 'There was an error sending the message.';
+    }
   });
