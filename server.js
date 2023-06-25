d0789cf6b374dfa449c7e743463c5feca09f9f24
Mshumsky1 <112615574+Mshumsky1@users.noreply.github.com>
2023-06-25 15:20:32 +0900

    Update server.js

--- server.js
+++ server.js
@@ -58,3 +58,8 @@ const port = 3000;
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
